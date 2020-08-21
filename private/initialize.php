519621a71c848b2dabe5838f461bca25fcc85564
SAAD ARROUB <30563109+saadarroub@users.noreply.github.com>
2020-08-21 17:41:48 +0100

    create cvs

--- private/initialize.php
+++ private/initialize.php
@@ -26,6 +26,20 @@
   
   // Load class definitions manually
 
+  // -> Individually
+  // require_once('classes/bicycle.class.php');
+
+  // -> All classes in directory
+  foreach(glob('classes/*.class.php') as $file) {
+    require_once($file);
+  }
+
   // Autoload class definitions
+  function my_autoload($class) {
+    if(preg_match('/\A\w+\Z/', $class)) {
+      include('classes/' . $class . '_class.php');
+    }
+  }
+  spl_autoload_register('my_autoload');
 
 ?>
--- public/bicycles.php
+++ public/bicycles.php
@@ -26,16 +26,22 @@
         <th>Price</th>
       </tr>
 
-      <tr>
-        <td>Brand</td>
-        <td>Model</td>
-        <td>Year</td>
-        <td>Category</td>
-        <td>Gender</td>
-        <td>Color</td>
-        <td>Weight</td>
-        <td>Condition</td>
-        <td>Price</td>
+      <?php
+
+$args = ['brand' => 'Trek', 'model' => 'Emonda', 'year' => 2017, 'gender' => 'Unisex', 'color' => 'black', 'category' => 'Road', 'weight_kg' => 1.5, 'price' => 1000.00];
+$bike = new Bicycle($args);
+
+?>
+       <tr>
+        <td><?php echo h($bike->brand); ?></td>
+        <td><?php echo h($bike->model); ?></td>
+        <td><?php echo h($bike->year); ?></td>
+        <td><?php echo h($bike->category); ?></td>
+        <td><?php echo h($bike->gender); ?></td>
+        <td><?php echo h($bike->color); ?></td>
+        <td><?php echo h($bike->weight_kg()) . ' / ' . h($bike->weight_lbs()); ?></td>
+        <td><?php echo h($bike->condition()); ?></td>
+        <td><?php echo h($bike->price); ?></td>
       </tr>
 
     </table>
